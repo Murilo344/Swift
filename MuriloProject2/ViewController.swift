1a49b913dec834a8ce9213c4fd1ff820ef375087
ICMMAC04-3F86 <icmmac04-3f86@ICMMAC04-3F86s-Mac-mini.local>
2022-07-20 10:33:59 -0400

    Auto layout

---
+++ MuriloProject2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MuriloProject2
+//
+//  Created by ICMMAC04-3F86 on 20/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
